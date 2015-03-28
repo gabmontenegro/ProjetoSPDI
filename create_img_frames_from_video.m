9d1342c5a7cbaf2a7dabe6ef15ff10a6c3136bab
Flávio Júnior <flavio.ipiranga@gmail.com>
2015-03-28 15:17:04 -0400

    A função agora recebe um intervalo de frames.

--- create_img_frames_from_video.m
+++ create_img_frames_from_video.m
@@ -1,13 +1,14 @@
-function [] = create_img_frames_from_video(folder_video, video_in, nFrames)
+function [] = create_img_frames_from_video(folder_video, video_in, in_Frame, end_Frame)
 
-video_obj = VideoReader(strcat(folder_video, video_in));
-frames_video_in = read(video_obj, [1 nFrames]);
+video_obj = VideoReader(fullfile(folder_video, '\', video_in));
+frames_video_in = read(video_obj, [in_Frame end_Frame]);
 
-mkdir(folder_video, sprintf('frames_%s', video_in));
+mkdir(folder_video, sprintf('frames_%s', video_in(1:(length(video_in)-4))));
 
-folder_frames = fullfile(folder_video, sprintf('frames_%s', video_in), '\');
-
-for n = 1:nFrames
-    imName = sprintf('frame%d_video_%s.jpg', n, video_in(1 length(video_in)-4)));
-    imwrite(frames_video_in(:, :, :, n),strcat(folder_frames,imName));
+folder_frames = fullfile(folder_video, sprintf('frames_%s', video_in(1:(length(video_in)-4))), '\');
+aux = 0;
+for n = in_Frame:end_Frame
+    aux = aux +1;
+    imName = sprintf('frame%d_video_%s.jpg', n, video_in(1:(length(video_in)-4)));
+    imwrite(frames_video_in(:, :, :, aux),strcat(folder_frames,imName));
 end
